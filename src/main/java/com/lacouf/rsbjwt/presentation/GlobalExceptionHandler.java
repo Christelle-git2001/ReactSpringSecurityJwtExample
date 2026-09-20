c675077b78f5da52ba7131bc26b3fac7d53491c2
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-20 03:42:10 -0400

    correction du global exception handler

--- src/main/java/com/lacouf/rsbjwt/presentation/GlobalExceptionHandler.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.lacouf.rsbjwt.presentation;
 
+import com.lacouf.rsbjwt.Exception.DepartementInvalideException;
 import com.lacouf.rsbjwt.Exception.EmailExistantException;
+import com.lacouf.rsbjwt.Exception.MatriculeExistantException;
 import com.lacouf.rsbjwt.Exception.MotDePasseNonCorrespondantException;
 import com.lacouf.rsbjwt.service.dto.ErreurDTO;
 import org.springframework.http.HttpStatus;
@@ -10,17 +12,33 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
     @ExceptionHandler(EmailExistantException.class)
-    public ResponseEntity<ErreurDTO> handleConflit(Exception e) {
+    public ResponseEntity<ErreurDTO> handleEmailExistantException(EmailExistantException e) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErreurDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(MatriculeExistantException.class)
+    public ResponseEntity<ErreurDTO> handleMatriculeExistantException(MatriculeExistantException e) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(new ErreurDTO(e.getMessage()));
     }
 
     @ExceptionHandler(MotDePasseNonCorrespondantException.class)
-    public ResponseEntity<ErreurDTO> handleBadRequest(Exception e) {
+    public ResponseEntity<ErreurDTO> handleMotDePasseNonCorrespondantException(MotDePasseNonCorrespondantException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErreurDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(DepartementInvalideException.class)
+    public ResponseEntity<ErreurDTO> handleDepartementInvalideException(DepartementInvalideException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(new ErreurDTO(e.getMessage()));
     }
 }
+
