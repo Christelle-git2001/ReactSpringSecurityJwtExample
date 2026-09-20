e8b440e14e83bb2cd19f4b627c7a0da50f377d90
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-20 02:48:13 -0400

    test controller doit inscrire professeur

--- src/test/java/com/lacouf/rsbjwt/presentation/ProfesseurControllerTest.java
+++ src/test/java/com/lacouf/rsbjwt/presentation/ProfesseurControllerTest.java
@@ -1,4 +1,85 @@
 package com.lacouf.rsbjwt.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lacouf.rsbjwt.service.ProfesseurService;
+import com.lacouf.rsbjwt.service.dto.InscriptionEtudiantDTO;
+import com.lacouf.rsbjwt.service.dto.InscriptionProfesseurDTO;
+import com.lacouf.rsbjwt.service.dto.ProfesseurDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("test")
 public class ProfesseurControllerTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockitoBean
+    private ProfesseurService professeurService;
+
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper;
+
+    private InscriptionProfesseurDTO inscriptionProfesseurDTO;
+    private ProfesseurDTO professeurDTO;
+
+    @BeforeEach
+    void init() {
+
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+
+        objectMapper = new ObjectMapper();
+
+        inscriptionProfesseurDTO = new InscriptionProfesseurDTO(
+                "Carole",
+                "Test",
+                "carole.test@gmail.com",
+                "111111",
+                "111111",
+                "1234567",
+                "123-123-1234",
+                "INFORMATIQUE"
+        );
+
+        professeurDTO = new ProfesseurDTO(
+                1L,
+                "Carole",
+                "Test",
+                "carole.test@gmail.com",
+                "1234567",
+                "123-123-1234",
+                "INFORMATIQUE"
+        );
+    }
+
+    @Test
+    void doitInscrireProfesseur() throws Exception {
+        when(professeurService.inscrireProfesseur(any(InscriptionProfesseurDTO.class)))
+                .thenReturn(professeurDTO);
+
+        mockMvc.perform(post("/professeurs/inscription")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inscriptionProfesseurDTO)))
+                .andExpect(status().isCreated());
+    }
+
 }
