7a569f197f57a3fc68fedd642f78275087e14948
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-21 04:58:55 -0400

    ajout de l'exception numeroTelephoneExistatnt

--- src/main/java/com/lacouf/rsbjwt/presentation/ProfesseurController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/ProfesseurController.java
@@ -1,12 +1,10 @@
 package com.lacouf.rsbjwt.presentation;
 
-import com.lacouf.rsbjwt.Exception.DepartementInvalideException;
-import com.lacouf.rsbjwt.Exception.EmailExistantException;
-import com.lacouf.rsbjwt.Exception.MatriculeExistantException;
-import com.lacouf.rsbjwt.Exception.MotDePasseNonCorrespondantException;
+import com.lacouf.rsbjwt.Exception.*;
 import com.lacouf.rsbjwt.service.ProfesseurService;
 import com.lacouf.rsbjwt.service.dto.InscriptionProfesseurDTO;
 import com.lacouf.rsbjwt.service.dto.ProfesseurDTO;
+import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -28,11 +26,11 @@ public class ProfesseurController {
     }
 
     @PostMapping("inscription")
-    public ResponseEntity<ProfesseurDTO> creerCompteProfesseur(@RequestBody InscriptionProfesseurDTO newProfesseur)
+    public ResponseEntity<ProfesseurDTO> creerCompteProfesseur(@Valid @RequestBody InscriptionProfesseurDTO newProfesseur)
             throws DepartementInvalideException,
             EmailExistantException,
             MatriculeExistantException,
-            MotDePasseNonCorrespondantException{
+            MotDePasseNonCorrespondantException, NumeroTelephoneExistantException {
 
             ProfesseurDTO createdProfesseur = professeurService.creerCompteProfesseur(newProfesseur);
             logger.info("{}", newProfesseur);
