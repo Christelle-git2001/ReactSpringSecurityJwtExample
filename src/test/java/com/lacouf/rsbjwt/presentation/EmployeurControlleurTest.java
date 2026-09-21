324211a8264caefbd70e9a26b5a3624a97c5b4f9
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-21 05:32:15 -0400

    ajout de ID dans le DTO de sortie et ajsutement du code en fonction

--- src/test/java/com/lacouf/rsbjwt/presentation/EmployeurControlleurTest.java
+++ src/test/java/com/lacouf/rsbjwt/presentation/EmployeurControlleurTest.java
@@ -61,6 +61,7 @@ public class EmployeurControlleurTest {
     @Test
     void inscriptionEmployeurCreated() throws Exception {
       EmployeurDTO employeurDTO = new EmployeurDTO(
+              1L,
               "Gerard",
               "Robert",
               "165-685-4569",
