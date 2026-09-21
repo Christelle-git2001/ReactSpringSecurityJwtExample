f583204b1aff07d5a38741bce4639a6812402ba8
Cindy Belle <cindylaelbelle@gmail.com>
2026-09-21 11:14:18 -0400

    DEBUG : changer le url de config.jsx pour empêcher erreur CORS

--- react-vite-project/src/api/professeur.jsx
+++ react-vite-project/src/api/professeur.jsx
@@ -1,7 +1,7 @@
 import { fetchJson } from "./http.jsx";
 
 export function inscrireProfesseur(professeur) {
-    return fetchJson("professeur/inscription", {
+    return fetchJson("/professeur/inscription", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(professeur)
