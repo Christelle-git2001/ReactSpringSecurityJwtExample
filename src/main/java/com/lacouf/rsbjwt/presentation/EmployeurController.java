5cc14c83b8b8e347a0139eb81cfa73a054b017a3
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-24 06:56:01 -0400

    connexion au back end de employeur

--- src/main/java/com/lacouf/rsbjwt/presentation/EmployeurController.java
+++ src/main/java/com/lacouf/rsbjwt/presentation/EmployeurController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/employeur")
-@CrossOrigin(origins = "http://localhost:5173")
 public class EmployeurController {
     private final EmployeurService employeurService;
 
