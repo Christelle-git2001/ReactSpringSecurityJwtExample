e7c46f2baeb90957122445acc5b847fba6b305c3
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2026-09-26 06:44:19 -0400

    suppression des execptions non utilises

--- src/main/java/com/lacouf/rsbjwt/Exception/EmailInvalideException.java
+++
@@ -1,7 +0,0 @@
-package com.lacouf.rsbjwt.Exception;
-
-public class EmailInvalideException extends RuntimeException {
-    public EmailInvalideException(String message) {
-        super("error.email_invalid");
-    }
-}
--- src/main/java/com/lacouf/rsbjwt/Exception/TelephoneInvalideException.java
+++
@@ -1,7 +0,0 @@
-package com.lacouf.rsbjwt.Exception;
-
-public class TelephoneInvalideException extends Exception {
-    public TelephoneInvalideException(String message) {
-        super("error.phone_invalid");
-    }
-}
